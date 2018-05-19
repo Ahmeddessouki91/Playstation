b6c0b656a698fe7c554d8c4f03c8786a41d039e4
Ahmed dessouki <eng.ahmed.dessouki91@gmail.com>
2018-05-19 23:30:51 +0200

    fix save game

--- src/app/admin/game-form/game-form.component.ts
+++ src/app/admin/game-form/game-form.component.ts
@@ -1,25 +1,33 @@
 import { GameService } from './../../services/app-services/game.service';
 import { CategoryService } from './../../services/app-services/category.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-form',
   templateUrl: './game-form.component.html',
   styleUrls: ['./game-form.component.css']
 })
-export class GameFormComponent implements OnInit {
+export class GameFormComponent implements OnInit, OnDestroy {
+
   catogories$;
+  subscribtion: Subscription;
 
   constructor(private categoryService: CategoryService, private gameService: GameService) {
-    this.catogories$ = categoryService.getCategories();
-    console.log(this.catogories$);
   }
 
   ngOnInit() {
+    this.catogories$ = this.categoryService.getCategories();
+    console.log(this.catogories$);
   }
 
   save(game) {
-    this.gameService.create(game);
+    this.subscribtion.add(this.gameService.create(game).subscribe(res => {
+      console.log(res);
+    }));
   }
 
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
 }
